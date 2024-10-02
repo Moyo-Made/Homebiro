436309a72aa3e8b258c22d082e0899eb5b2a2392
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-10-02 14:05:41 +0100

    added other components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import Hero from './components/Hero'
+import React from "react";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Waitlist from "./components/Waitlist";
+import Team from "./components/Team";
+import BuyMeCoffee from "./components/BuyMeCoffee";
+import Footer from "./components/Footer";
 
 const page = () => {
-  return (
-    <div><Hero /></div>
-  )
-}
+	return (
+		<div>
+			<Hero />
+			<Features />
+			<Waitlist />
+			<Team />
+			<BuyMeCoffee />
+			<Footer />
+		</div>
+	);
+};
 
-export default page
+export default page;
